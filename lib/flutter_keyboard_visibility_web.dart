89ac8cafcf29837abf5baa32727cba0ffc9c27bd
Amit sharan <amitsharan120977@gmail.com>
2025-07-20 19:19:30 +0530

    code updated

--- lib/flutter_keyboard_visibility_web.dart
+++ lib/flutter_keyboard_visibility_web.dart
@@ -1,4 +1,5 @@
-import 'dart:html' as html show window, Navigator;
+import 'dart:async';
+import 'package:web/web.dart';
 import 'package:flutter_keyboard_visibility_platform_interface/flutter_keyboard_visibility_platform_interface.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
@@ -7,19 +8,48 @@ import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 class FlutterKeyboardVisibilityPluginWeb
     extends FlutterKeyboardVisibilityPlatform {
   /// Constructs a [FlutterKeyboardVisibilityPluginWeb].
-  FlutterKeyboardVisibilityPluginWeb(html.Navigator navigator);
+  FlutterKeyboardVisibilityPluginWeb(Navigator navigator);
+  static final _onChangeController =
+      StreamController<KeyboardVisibilityStatus>();
+  static final _onChange = _onChangeController.stream.asBroadcastStream();
 
   /// Factory method that initializes the FlutterKeyboardVisibility plugin
   /// platform with an instance of the plugin for the web.
   static void registerWith(Registrar registrar) {
     FlutterKeyboardVisibilityPlatform.instance =
-        FlutterKeyboardVisibilityPluginWeb(html.window.navigator);
+        FlutterKeyboardVisibilityPluginWeb(window.navigator);
+    EventStreamProvider<Event>(
+      'resize',
+    ).forTarget(window.visualViewport).listen((e) {
+      final minKeyboardHeight = 200;
+
+      final screenHeight = window.screen.height;
+      final viewportHeight = window.visualViewport?.height.toInt() ?? 0;
+      final newState = (screenHeight - minKeyboardHeight) > viewportHeight;
+      if (newState) {
+        _updateValue(KeyboardVisibilityStatus.visible);
+      } else {
+        _updateValue(KeyboardVisibilityStatus.notVisible);
+      }
+    });
+  }
+
+  static KeyboardVisibilityStatus get isVisible => _isVisible;
+  static KeyboardVisibilityStatus _isVisible =
+      KeyboardVisibilityStatus.notVisible;
+
+  static void _updateValue(KeyboardVisibilityStatus newValue) {
+    // Don't report the same value multiple times
+    if (newValue == _isVisible) {
+      return;
+    }
+
+    _isVisible = newValue;
+    _onChangeController.add(newValue);
   }
 
   /// Emits changes to keyboard visibility from the platform. Web is not
-  /// implemented yet so false is returned.
+  /// implemented yet so `notVisible` is returned.
   @override
-  Stream<bool> get onChange async* {
-    yield false;
-  }
+  Stream<KeyboardVisibilityStatus> get onChange => _onChange;
 }
